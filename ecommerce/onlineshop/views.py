fd42c8427d55fa2c1bed481ec1c5c0d0ebf01d54
furqandar <furqandarsf@gmail.com>
2020-01-13 10:20:46 +0500

    create bootstrap file, static file and link

--- ecommerce/onlineshop/views.py
+++ ecommerce/onlineshop/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
-def index (request):
-    return HttpResponse('index shop')
+def index(request):
+    return render(request,'shop/index.html')
